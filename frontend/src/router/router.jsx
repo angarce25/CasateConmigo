9abbe08b52083514d0eef1ef5ba24ae4d9e11786
elenalope <elopmorales97@gmail.com>
2024-02-21 12:53:56 +0100

    router

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,11 +1,26 @@
-import Footer from "../components/footer/Footer";
 import { createBrowserRouter } from "react-router-dom"; 
+import Footer from "../components/footer/Footer";
 
 export const router = createBrowserRouter ([
     {
+        path: "/navbar",
+        element: "<Navbar/>",
+    },
+    {
+        path: "/searchBar",
+        element: "<SearchBar/>",
+    },
+    {
+        path: "/home",
+        element: "<Home/>",
+    },
+    {
+        path: "/contact",
+        element: "<Contact/>",
+    },
+    {
         path: "/footer",
         element: "<Footer/>",
-
     }
 ]) ;
 
