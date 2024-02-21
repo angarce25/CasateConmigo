8f4101f8ed2f720270cf2f8daf74d52ca06d99c4
= <=>
2024-02-21 13:27:19 +0100

    router nav y footer

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,12 +1,18 @@
-import Footer from "../components/footer/Footer";
-import { createBrowserRouter } from "react-router-dom"; 
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar'; // Importa tu componente Navbar
+import Footer from '../components/footer/Footer'; // Importa tu componente Footer
 
-export const router = createBrowserRouter ([
-    {
-        path: "/footer",
-        element: "<Footer/>",
-
-    }
-]) ;
+// Crea el objeto de ruta utilizando createBrowserRouter
+export const router = createBrowserRouter([
+  {
+    path: '/', // Define la ruta para el Navbar
+    element: <Navbar />, // Asigna tu componente Navbar al elemento
+  },
+  {
+    path: '/footer', // Define la ruta para el Footer
+    element: <Footer />, // Asigna tu componente Footer al elemento
+  },
+]);
 
 export default router;
