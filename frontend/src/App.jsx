88c8519f2ed863e2e3dd630e4808c7d37f0456de
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-02-21 15:16:31 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+
 import SearchBar from './components/searchBar/searchBar'
 
 function App() {
